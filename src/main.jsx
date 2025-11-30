bb23c353fcee7fdf83e6fc4bff638666fbaf4295
md-alif007 <mohammadalif043@gmail.com>
2025-11-30 11:30:47 +0600

    adding components and element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import About from './Components/About/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path:"About",
+    element : <About></About>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
